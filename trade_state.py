1c150f54b391583969f298861f0505f15f1e7608
Eladio <eladio.rocha99@gmail.com>
2023-05-18 23:04:22 -0600

    Handle the current trade state

--- trade_state.py
+++ trade_state.py
@@ -4,16 +4,24 @@ class TradeState:
     It holds information about the trade's balance, equity, signal event, and other relevant data.
 
     Attributes:
-        signal_event (SignalEvent): An instance of the SignalEvent class associated with the trade.
         balance (float): The initial account balance for the trade.
         equity (float): The account equity, initially set to the balance.
         state (str, optional): The current state of the trade (e.g., 'OPEN', 'CLOSED', etc.). Defaults to None.
-        amount (float): The amount of the security being traded. Defaults to 0.
+        size (float): The size of the security being traded. Defaults to 0.
         unrealized_profit (float): The current unrealized profit for the trade. Defaults to 0.
         realized_profit (float): The realized profit for the trade. Defaults to 0.
     """
 
-    def __init__(self, balance, signal_event):
+    def __init__(
+        self, 
+        balance,
+        size=0, 
+        signal=None, 
+        stop_loss=0, 
+        take_profit=0, 
+        adjusted_price=0,
+        entry_price=0,
+    ):
         """
         Initializes a new instance of the TradeState class.
 
@@ -21,16 +29,14 @@ class TradeState:
             balance (float): The initial account balance for the trade.
             signal_event (SignalEvent): An instance of the SignalEvent class associated with the trade.
         """
-        self.signal_event = signal_event
+        self.timestamp = None
+        self.adjusted_price = adjusted_price
+        self.stop_loss = stop_loss
+        self.take_profit = take_profit
         self.balance = balance
         self.equity = balance
-        self.state = None
-        self.amount = 0
+        self.signal = signal
+        self.size = size
         self.unrealized_profit = 0
         self.realized_profit = 0
-
-    def __str__(self):
-        return "Trade State: %s, Balance: %s, Equity: %s, Amount: %s, Unrealized Profit: %s, Realized Profit: %s" % (self.state, self.balance, self.equity, self.amount, self.unrealized_profit, self.realized_profit)
-
-    def __repr__(self):
-        return str(self)
+        self.entry_price = entry_price
