08cd0961de0a993496e0ca58c49c7a776a2af85a
Eladio <eladio.rocha99@gmail.com>
2023-05-28 18:09:01 -0600

    Make 'symbol' parameter optional

--- backtestify/cfd.py
+++ backtestify/cfd.py
@@ -1,7 +1,7 @@
 from backtestify.financial_instrument import FinancialInstrument
 
 class CFD(FinancialInstrument):
-    def __init__(self, symbol, instrument_type, lot_size, entry_lots, commission, point_value, leverage, period, point, spread, pips, currency_ratio=1):
+    def __init__(self, instrument_type, lot_size, entry_lots, commission, point_value, leverage, period, point, spread, pips, currency_ratio=1, symbol=None):
         super().__init__(symbol, lot_size, entry_lots, commission, leverage, period, currency_ratio)
         self.instrument_type = instrument_type
         self.pips = pips
