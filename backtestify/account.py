1212edb0f4a910b16c44296bbdc545ad849654ea
Eladio <eladio.rocha99@gmail.com>
2023-06-06 11:24:43 -0600

    Add setter methods to update balance and equity

--- backtestify/account.py
+++ backtestify/account.py
@@ -3,3 +3,8 @@ class Account:
         self.balance = initial_balance
         self.equity = self.balance
         
+    def set_balance(self, balance):
+        self.balance = balance
+
+    def set_equity(self, equity):
+        self.equity = equity
