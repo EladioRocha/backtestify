95a2e341c3f039c806d3824cf978a64883e53e3e
Eladio <eladio.rocha99@gmail.com>
2023-06-06 11:04:49 -0600

    Version 0.1.5

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 import setuptools
 
 with open("README.md", "r") as file:
-    more_description = file.read()
+    readme = file.read()
 
 setuptools.setup(
     name="backtestify",
-    version="0.1.4",
+    version="0.1.5",
     author="Eladio Rocha Vizcaino",
     author_email="eladio.rocha99@gmail.com",
     description="A package for backtesting trading strategies",
-    more_description=more_description,
+    long_description=readme,
     long_description_content_type="text/markdown",
     url="https://github.com/EladioRocha/backtestify",
     project_urls={
